0eb6631f1c5b21c72a814a573de4b736d5c0dcd0
Albarelli <felipesalbarelli@gmail.com>
2024-03-18 23:15:49 -0300

    add tile e clear em cache de file upload

--- src/app/comps/tiles-side-bar/tiles-side-bar.component.ts
+++ src/app/comps/tiles-side-bar/tiles-side-bar.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, contentChild, effect, signal } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { FileUpload, FileUploadEvent, FileUploadHandlerEvent, FileUploadModule } from 'primeng/fileupload';
+
 
 @Component({
   selector: 'app-tiles-side-bar',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule, FileUploadModule],
   templateUrl: './tiles-side-bar.component.html',
   styleUrl: './tiles-side-bar.component.scss'
 })
 export class TilesSideBarComponent {
+
+  // fileBtn = contentChild<FileUpload>(FileUpload)
+
+  // e = effect( () => {
+  //   console.log(this.fileBtn())
+  // })
+
+  files = signal<{
+    file: File,
+    url: string
+  }[]>([])
+
+  onUpload(event: FileUploadHandlerEvent , ele: FileUpload) {
+    const file = event.files[0] 
+    const url = URL.createObjectURL(file)
+    console.log({img: file, url})
+    this.files.update( (prev) => ([...prev , {url, file}]) )
+    ele.clear()
+    return
+  }
+
+
 
 }
